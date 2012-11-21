7c79bc8323b1dd57da9d11adaf851a23f0a63c0c
andypaxo <andy@andypaxo.net>
2012-11-20 21:41:35 -0700

    Add and retrieve scores via web interface

--- lib/web.rb
+++ lib/web.rb
@@ -1,9 +1,20 @@
 # This line adds the current file's location to Ruby's path
 $LOAD_PATH.unshift(File.dirname(__FILE__))
+
 require 'sinatra'
 require 'scoreboard'
 
+scores = Scoreboard.new
 
 get '/' do
-	return 'Hi there!'
+	return scores.player_stats.to_s
+end
+
+get '/add/:name/:game/:score' do
+	scores.add({
+		:name => params[:name],
+		:game => params[:game],
+		:score => params[:score]
+	})
+	return 'Added score'
 end

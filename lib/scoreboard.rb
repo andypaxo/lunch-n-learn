4f73c4d29142dcde6e6e8948a42cc94c9c98248b
andypaxo <andy@andypaxo.net>
2012-11-22 12:44:23 -0700

    flesh out the repository class

--- lib/scoreboard.rb
+++ lib/scoreboard.rb
@@ -1,2 +1,19 @@
 class Scoreboard
+	
+	def initialize
+		@cards = []
+	end
+
+	def add(scorecard)
+		@cards.push(scorecard)
+	end
+	
+	def all
+		@cards
+	end
+	
+	def cards_by_name
+		@cards.group_by { |card| card.name }
+	end
+	
 end
--- spec/scoreboard_spec.rb
+++ spec/scoreboard_spec.rb
@@ -8,5 +8,18 @@ describe 'scoreboard' do
 	
 	it 'Should save and retrieve cards' do
 		@sut.add(Scorecard.new('Andy','Elder Scrolls','Level 60'))
+		
+		first_card = @sut.all[0]
+		first_card.name.should eq('Andy')
+	end
+	
+	it 'Should group cards by name' do
+		@sut.add(Scorecard.new('Andy','Elder Scrolls','Level 60'))
+		@sut.add(Scorecard.new('Regis','WoW','Level 60'))
+		@sut.add(Scorecard.new('Regis','LoTR','Level 54'))
+		
+		all_cards = @sut.cards_by_name
+		regis_cards = all_cards['Regis']
+		regis_cards.count.should eq(2)
 	end
 end
